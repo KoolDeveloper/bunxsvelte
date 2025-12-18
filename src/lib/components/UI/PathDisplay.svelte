dfd50a4dd0b54e9cedfed4a36044da75bf34bfe4
Codernetix <james.ruiz@cibernetix.com.co>
2025-12-18 15:14:17 -0500

    created file structure

--- src/lib/components/UI/PathDisplay.svelte
+++ src/lib/components/UI/PathDisplay.svelte
@@ -3,7 +3,16 @@
     let {path} = $props();
 
 </script>
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=BBH+Bogle&family=Bree+Serif&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap');
+
+    .font-bree{
+        font-family: 'Bree Serif', serif;
+    }
+
+</style>
+
 
 <div class="flex">
-    <h2 class="text-2xl pl-4 uppercase backdrop-brightness-125 text-[#FFFF4D]">{path}</h2>
+    <h2 class="text-2xl pl-4 uppercase backdrop-brightness-125 text-[#FFFF4D] font-bree">{path}</h2>
 </div>
